055f79450bdb5db7f00f9961cb5fe9b563304e01
Duamajeed00 <mduaa0976@gmail.com>
2024-10-03 23:28:12 +0500

    pushing code

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true, // This is optional and only if you're using the `/app` directory
+    },
+  };
+  
